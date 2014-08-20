99d5b03ea33b795efc8724c78acd04a6918c1fd4
tt07406 <tuchengjun@gmail.com>
2014-08-21 00:29:31 +0800

    struts1.1
    完成了登录，权限管理，新闻类别管理的struts实现

---
+++ Stock/src/org/news/action/AdminLogin.java
@@ -0,0 +1,120 @@
+/*
+ * 系统名称：新闻发布系统
+ * 
+ * 类名：AdminLogin
+ * 
+ * 创建日期：2014-08-18
+ */
+package org.news.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.news.model.Admin;
+import org.news.service.AdminService;
+import org.news.utils.MD5Code;
+import org.news.utils.MessageUtil;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 用于管理员登录的Action
+ * 
+ * @author tt
+ * @version 14.8.18
+ */
+public class AdminLogin extends ActionSupport {
+
+	private static final long serialVersionUID = 2124111050903078657L;
+	
+	private String adminName;
+	private String code;
+	private String password;
+
+	/**
+	 * @return the adminName
+	 */
+	public String getAdminName() {
+		return adminName;
+	}
+
+	/**
+	 * @param adminName the adminName to set
+	 */
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return
+	 */
+	public String execute() {
+		ActionContext ctx = ActionContext.getContext();
+		List<String> errors = new ArrayList<String>();//错误列表
+		
+		if (code == null || "".equals(code)) {//验证码为空
+			errors.add(MessageUtil.get("adminlogin.code.null"));
+		} else {
+			String rand = (String) ctx.getSession().get("rand"); // 取得生成的验证码
+			if (!code.equalsIgnoreCase(rand)) {//验证码不匹配
+				errors.add(MessageUtil.get("adminlogin.code.err")) ;
+			}
+		}
+		
+		if (adminName == null || "".equals(adminName)) {//用户名为空
+			errors.add(MessageUtil.get("adminlogin.adminid.null"));
+		}
+		
+		if (password == null || "".equals(password)) {//密码为空
+			errors.add(MessageUtil.get("adminlogin.password.null"));
+		}
+		
+		if(errors.size()==0){	// 现在没有任何的错误信息
+			Admin admin = new Admin(0,adminName,new MD5Code().getMD5ofStr(password),"Super Admin") ;
+			AdminService adminService = new AdminService(); //调用下一层的服务进行登录
+			try {
+				if(adminService.findLogin(admin)){//登录成功
+					admin = adminService.findAdminById(adminName);//从数据库获取对象
+					ctx.getSession().put("admin", admin);// 现在保存对象
+					return SUCCESS;
+				} else {
+					errors.add(MessageUtil.get("adminlogin.false")) ;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		ctx.put("errors", errors);//保存错误
+		
+		return ERROR;
+	}
+}
---
+++ Stock/src/org/news/utils/Pools.java
@@ -0,0 +1,133 @@
+/*
+ * 系统名称：新闻发布系统
+ * 
+ * 类名：Pools
+ * 
+ * 创建日期：2014-06-18
+ */
+package org.news.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.PropertyResourceBundle;
+
+/**
+ * JDBC连接池,当系统需要获取数据库数据时，只需创建一个数据库连接对象，
+ * 便可与数据库获取连接，然后可以执行SQL语句对数据库进行操作。
+ * @author tt
+ * @version 14.6.18
+ */
+public class Pools {
+
+	private final static int num = 3;//池中连接数量
+	/**
+	 * 数据库访问URL
+	 */
+	private static String url;
+
+	/**
+	 * 数据库驱动
+	 */
+	private static String driver;
+
+	/**
+	 * 数据库访问用户名
+	 */
+	private static String user;
+
+	/**
+	 * 数据库访问口令
+	 */
+	private static String password;
+
+	/**
+	 * 配置文件名称
+	 */
+	private final static String fileName = "database";	
+	private static Connection con = null;
+	private static LinkedList<Connection> pools = new LinkedList<Connection>();//用链表表示连接池
+	
+//	static{//初始化连接池，建立若干个连接
+//		config();
+//		try {
+//			Class.forName(driver);
+//			for(int i = 1 ; i <= num ; i++){
+//				pools.addLast(getPoolsConnection());
+//			}
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
+	public static void config() {
+		//读取系统配置
+		PropertyResourceBundle resourceBundle = (PropertyResourceBundle) PropertyResourceBundle
+				.getBundle(fileName);
+		//将系统设置赋值给类变量
+		Enumeration<String> enu = resourceBundle.getKeys();
+		while (enu.hasMoreElements()) {
+			String propertyName = enu.nextElement().toString();
+			if (propertyName.equals("database.url"))
+				url = resourceBundle.getString("database.url");
+			if (propertyName.equals("database.driver"))
+				driver = resourceBundle.getString("database.driver");
+			if (propertyName.equals("database.username"))
+				user = resourceBundle.getString("database.username");
+			if (propertyName.equals("database.password"))
+				password = resourceBundle.getString("database.password");
+		}
+		try {//初始化连接池，建立若干个连接
+			Class.forName(driver);
+			for(int i = 1 ; i <= num ; i++){
+				pools.addLast(getPoolsConnection());
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void closeAllConnections(){
+		try {
+			for(int i = 1 ; i <= num ; i++){
+				pools.get(i).close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 获取一个连接
+	 * @return
+	 */
+	public static Connection getPoolsConnection(){
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	/**
+	 * 从池中得到一个连接
+	 * @return
+	 */
+	public static Connection getPoolsConnectionSingle(){
+		con = pools.removeFirst();
+		return con ;
+	}
+	
+	/**
+	 * 将连接放回池中
+	 * @param con
+	 * @throws SQLException
+	 */
+	public static void freeConnection(Connection con) throws SQLException{
+		pools.addLast(con);
+	}
+	
+}
